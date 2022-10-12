58be4708e3304531ac8c325d25a92440e54a6f32
Matheus Henrique Nascimento <110841402+Matheus-Henrique-Nascimento@users.noreply.github.com>
2022-10-12 19:50:02 -0300

    Preparando o terreno para o Javascript.
    Mudei as variáveis — colocando-as no :root e não em sua forma sass — para alterá-las via javascript. Também coloquei alguns escutadores de eventos no html.

---
+++ logic.js
@@ -0,0 +1,17 @@
+/* ------ Botões gerais. */
+
+function acessibilidade(){
+    window.alert("Acessibilidade.");
+}
+
+function tradutor(){
+    window.alert("Tradução.");
+}
+
+function modo_noturno(){
+    window.alert("Aqui estará a lógica que tornará o site escuro.");
+}
+
+function modo_diurno(){
+    window.alert("Aqui estará a lógica que o retornará a ser claro.");
+}
