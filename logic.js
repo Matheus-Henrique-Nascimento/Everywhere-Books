d7b08078f7e014775f663309d3cca5e94b9cfe75
Matheus Henrique Nascimento <110841402+Matheus-Henrique-Nascimento@users.noreply.github.com>
2022-10-13 14:24:43 -0300

    Finalizando a estilização do interruptor.

--- logic.js
+++ logic.js
@@ -13,6 +13,9 @@ function modo_noturno(){
     document.querySelector(":root").style.setProperty("--cor-terceária", "#333333");
     document.querySelector(":root").style.setProperty("--cor-quaternária", "#A3A3A3");
     document.querySelector(":root").style.setProperty("--cor-quaternária-opaca", "#A3A3A380");
+
+    document.querySelector("#main-botões-interruptor").querySelectorAll(".material-icons-outlined")[0].style.backgroundColor = "var(--cor-primária)";
+    document.querySelector("#main-botões-interruptor").querySelectorAll(".material-icons-outlined")[1].style.backgroundColor = "var(--cor-terceária)";
 }
 
 function modo_diurno(){
@@ -20,4 +23,7 @@ function modo_diurno(){
     document.querySelector(":root").style.setProperty("--cor-terceária", "#FFFFFB");
     document.querySelector(":root").style.setProperty("--cor-quaternária", "#171717");
     document.querySelector(":root").style.setProperty("--cor-quaternária-opaca", "#17171780");
+
+    document.querySelector("#main-botões-interruptor").querySelectorAll(".material-icons-outlined")[1].style.backgroundColor = "var(--cor-primária)";
+    document.querySelector("#main-botões-interruptor").querySelectorAll(".material-icons-outlined")[0].style.backgroundColor = "var(--cor-terceária)";
 }
