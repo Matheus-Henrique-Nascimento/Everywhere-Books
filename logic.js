4051f086828ae49e1306f0a24786356790731b13
Matheus Henrique Nascimento <110841402+Matheus-Henrique-Nascimento@users.noreply.github.com>
2022-10-18 20:35:31 -0300

    Terminando o efeito de máquina de escrever e mexendo no efeito e ruído e papel.

--- logic.js
+++ logic.js
@@ -2,21 +2,19 @@
 
 /* ------ Altura responsiva dos efeitos. */
 
-window.addEventListener('load', (event) => {
-    setInterval(efeitos_ruídos_papel, 10);
+window.addEventListener('load', () => {
+    setInterval(() => {
+        document.querySelector("#ruído-entrada-inicial").style.height = `${parseInt(window.getComputedStyle(document.querySelector("#main-botões-gerais")).height) + parseInt(window.getComputedStyle(document.querySelector("#main-entrada-inicial")).height)}px`;
+        document.querySelector("#ruído-newsletter").style.height = `${parseInt(window.getComputedStyle(document.querySelector("#main-newsletter")).height)}px`;
+        document.querySelector("#ruído-promoção").style.height = `${parseInt(window.getComputedStyle(document.querySelector("#main-promoção")).height)}px`;
+        document.querySelector("#papel").style.height = `${parseInt(window.getComputedStyle(document.querySelector("footer")).height)}px`;
+    }, 10);
 });
-
-function efeitos_ruídos_papel(){
-    document.querySelector("#ruído-entrada-inicial").style.height = `${parseInt(window.getComputedStyle(document.querySelector("#main-botões-gerais")).height) + parseInt(window.getComputedStyle(document.querySelector("#main-entrada-inicial")).height)}px`;
-    document.querySelector("#ruído-newsletter").style.height = `${parseInt(window.getComputedStyle(document.querySelector("#main-newsletter")).height)}px`;
-    document.querySelector("#ruído-promoção").style.height = `${parseInt(window.getComputedStyle(document.querySelector("#main-promoção")).height)}px`;
-    document.querySelector("#papel").style.height = `${parseInt(window.getComputedStyle(document.querySelector("footer")).height)}px`;
-}
 
 /* ------ Tradução. */
 
 function tradutor(){
-    window.alert("Tradução.");
+    window.alert("Aqui haverá a tradução do site.");
 }
 
 /* ------ Interruptor de cores. */
@@ -46,9 +44,35 @@ window.addEventListener('load', (event) => {
 });
 
 function máquina_de_escrever(){
-    const texto = document.querySelector("#main-entrada-inicial-informações").querySelector("h2").querySelector("i").textContent.split("");
+    const texto_entrada_inicial = document.querySelector("#main-entrada-inicial-informações").querySelector("h2").querySelector("i").textContent.split("");
+    const texto_newsletter = document.querySelector("#main-newsletter").querySelector("h2").querySelector("i").textContent.split("");
+    const texto_livros = document.querySelector("#main-carrossel-de-livros").querySelector("h2").querySelector("i").textContent.split("");
+    const texto_promoção = document.querySelector("#main-promoção").querySelector("h2").querySelector("i").textContent.split("");
+    const texto_artigos = document.querySelector("#main-carrossel-de-artigos").querySelector("h2").querySelector("i").textContent.split("");
+
     document.querySelector("#main-entrada-inicial-informações").querySelector("h2").querySelector("i").innerHTML = "";
-    texto.forEach((letra, i) => {
+    document.querySelector("#main-newsletter").querySelector("h2").querySelector("i").innerHTML = "";
+    document.querySelector("#main-carrossel-de-livros").querySelector("h2").querySelector("i").innerHTML = "";
+    document.querySelector("#main-promoção").querySelector("h2").querySelector("i").innerHTML = "";
+    document.querySelector("#main-carrossel-de-artigos").querySelector("h2").querySelector("i").innerHTML = "";
+
+    texto_entrada_inicial.forEach((letra, i) => {
         setTimeout(() => document.querySelector("#main-entrada-inicial-informações").querySelector("h2").querySelector("i").innerHTML += letra, 500 * i);
+    });
+
+    texto_newsletter.forEach((letra, i) => {
+        setTimeout(() => document.querySelector("#main-newsletter").querySelector("h2").querySelector("i").innerHTML += letra, 500 * i);
+    });
+
+    texto_livros.forEach((letra, i) => {
+        setTimeout(() => document.querySelector("#main-carrossel-de-livros").querySelector("h2").querySelector("i").innerHTML += letra, 500 * i);
+    });
+
+    texto_promoção.forEach((letra, i) => {
+        setTimeout(() => document.querySelector("#main-promoção").querySelector("h2").querySelector("i").innerHTML += letra, 500 * i);
+    });
+
+    texto_artigos.forEach((letra, i) => {
+        setTimeout(() => document.querySelector("#main-carrossel-de-artigos").querySelector("h2").querySelector("i").innerHTML += letra, 500 * i);
     });
 }
